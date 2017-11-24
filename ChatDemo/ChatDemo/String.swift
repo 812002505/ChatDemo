a2bff172863ece20a2c5ac68aee9349f6554537d
zyc <13061650152@163.com>
2017-11-24 20:06:45 +0800

    aa

--- ChatDemo/ChatDemo/String.swift
+++ ChatDemo/ChatDemo/String.swift
@@ -7,11 +7,21 @@
 //
 
 extension String{
+//    func sha1() -> String {
+//        let data = self.data(using: String.Encoding.utf8)!
+//        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+//        CC_SHA1(data.bytes, CC_LONG(data.length), &digest)
+//        let hexBytes = map(digest) { String(format: "%02hhx", $0) }
+//        return "".join(hexBytes)
+//    }
     func sha1() -> String {
-        let data = self.data(usingEncoding: String.Encoding.utf8)!
+        var data = self.data(using: String.Encoding.utf8)!
         var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
-        CC_SHA1(data.bytes, CC_LONG(data.length), &digest)
-        let hexBytes = map(digest) { String(format: "%02hhx", $0) }
-        return "".join(hexBytes)
+        CC_SHA1(&data, CC_LONG(data.count), &digest)
+        let output = NSMutableString(capacity: Int(CC_SHA1_DIGEST_LENGTH))
+        for byte in digest {
+            output.appendFormat("%02x", byte)
+        }
+        return output as String
     }
 }
